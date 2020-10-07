e9cc52a66c8e3520737296e7c05716309fe81f5b
Adrian Romero Muñoz <alum.aromerom@iesalixar.org>
2020-10-07 20:07:31 +0200

    Modificacion Bits

--- U2_INTRODUCCION_JS/T5/Ejercicio7/Ejercicio7.js
+++ U2_INTRODUCCION_JS/T5/Ejercicio7/Ejercicio7.js
@@ -1,7 +1,7 @@
-var dezplazamiento1 = 125 >> 8;
-var dezplazamiento2 = 40 << 4;
-var dezplazamiento3 = 25 >> 2;
-var dezplazamiento4 = 10 << 16;
+var dezplazamiento1 = 125 >> 3;
+var dezplazamiento2 = 40 << 2;
+var dezplazamiento3 = 25 >> 1;
+var dezplazamiento4 = 10 << 4;
 
 console.log("125 / 8 = " + dezplazamiento1);
 console.log("40 x 4 = " + dezplazamiento2);
