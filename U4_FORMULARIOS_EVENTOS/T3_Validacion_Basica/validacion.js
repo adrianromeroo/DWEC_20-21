3bc94aebb27b59ea34c54daeb7254b374fa60dc7
Adrian Romero Muñoz <alum.aromerom@iesalixar.org>
2020-12-02 21:26:39 +0100

    asd

--- U4_FORMULARIOS_EVENTOS/T3_Validacion_Basica/validacion.js
+++ U4_FORMULARIOS_EVENTOS/T3_Validacion_Basica/validacion.js
@@ -1,14 +1,47 @@
-window.onload=iniciar;
+window.onload = iniciar;
+function iniciar() {
+    document.getElementById("nombre_disco").addEventListener("click", validar, false);
+    document.getElementById("grupo_cantante").addEventListener("click", validar, false);
+    document.getElementById("ano_publicacion").addEventListener("click", validar, false);
+    document.getElementById("tipo_musica").addEventListener("click", validar, false);
+    document.getElementById("localizacion").addEventListener("click", validar, false);
+    document.getElementById("nombre_disco").addEventListener("click", validar, false);
+}
 
+function validarLocalizacion() { 
+    loc = document.getElementById("localizacion").value;
+    if (!isNaN(loc) && loc == "") {
+        return true;
+    }
+    alert ("El campo tiene que ser numérico o estar vacio");
+    return true;
+}
 
-function validacion() {
+function validarAnopublicacion {
+    ano = document.getElementById("ano_publicacion").value;
+    if () {
+
+    }
+}
+
+function campo20() {
     
     n_disco = document.getElementById("nombre_disco").value;
     gc_musica = document.getElementById("grupo_cantante").value;
+
     if (n_disco.length >= 20) {
         return true;
+    } else {
+        alert("El campo es menor a 20 caracteres");
+        return false;
     }
-    alert("El campo es menor a 20 caracteres");
-    return false;
+    
+    if (gc_musica.length >= 20) {
+        return true;
+    } else {
+        alert("El campo es menor a 20 caracteres");
+        return false;
+    }
     
 }
+
